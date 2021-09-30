0f4aa594e9b8f57e5917e1b5080cb0ec1416ce41
espmcas <85141027+espmcas@users.noreply.github.com>
2021-09-30 02:06:32 -0400

    Add files via upload

---
+++ tree.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+{
+    int n;
+    cout <<"Enter a positive number: " ;
+    cin >> n;
+       for (int r=1; r <= n; r++)
+    {
+        for (int a=n-r; a>0; a--)
+        cout <<" ";
+        for (int c=1; c<=r; c++)
+        {
+            cout<< "* ";
+        }
+        cout << endl;
+
+
+    }
+    return 0;
+}
